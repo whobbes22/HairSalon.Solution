295f0823630a94a0cf86e2306fbead4109870868
Joe W <whobbes22@gmail.com>
2023-05-19 11:51:59 -0700

    client side basic functionality

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -41,7 +41,33 @@ namespace HairSalon.Controllers
                                         
       return View(thisClient);
     }
+    public ActionResult delete(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
 
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      _db.Clients.Remove(thisClient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
+    public ActionResult Edit(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Clients.Update(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using System.Collections.Generic;
@@ -16,59 +17,7 @@ namespace HairSalon.Controllers
 
     public ActionResult Index()
     {
-       List<Stylist> model = _db.Stylists.ToList();
-       //_db.Stylists.ToList();
-      return View(model);
-    }
-
-    public ActionResult Create()
-    {
       return View();
     }
-
-    [HttpPost]
-    public ActionResult Create(Stylist stylist)
-    {
-      _db.Stylists.Add(stylist);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
-
-    // public ActionResult Details(int id)
-    // {
-    //   Stylist thisStylist = _db.Stylists
-    //                             // .Include(stylist => stylist.Clients)
-    //                             .FirstOrDefault(stylist => stylist.StylistId == id);
-    //   return View(thisStylist);
-    // }
-
-    // public ActionResult Delete(int id)
-    // {
-    //   Stylist thisStylist = _db.Stylists.FirstOrDefault(Stylist => Stylist.StylistId == id);
-    //   return View(thisStylist);
-    // }
-
-    // [HttpPost, ActionName("Delete")]
-    // public ActionResult DeleteConfirmed(int id)
-    // {
-    //   Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
-    //   _db.Stylists.Remove(thisStylist);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
-
-    // public ActionResult Edit(int id)
-    // {
-    //   Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
-    //   return View(thisStylist);
-    // }
-
-    // [HttpPost]
-    // public ActionResult Edit(Stylist stylist)
-    // {
-    //   _db.Stylists.Update(stylist);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
   }
 }
